4747dfa4f5b2ef94ebd4613e81bf4e605a11013c
[SW4/1반] 정혜린 <flsgp0317@gmail.com>
2023-11-15 01:10:09 +0900

    feat Promise resolve후에 Input값 set하는 방식으로 변경

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -9,16 +9,18 @@ interface DebounceOptions {
 function App() {
   const [inputValue, setInputValue] = useState<string>('기본값입니다');
 
+  //wait은 지연시간, immediate는 즉시실행 유무
   const debounceUserInput = debounce(
     (e: ChangeEvent<HTMLInputElement>) => {
-      setInputValue(e.target.value);
+      return e.target.value;
     },
     { wait: 1000, immediate: false } as DebounceOptions
   );
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     debounceUserInput.debounced(e).then(() => {
-      console.log('promise fullfilled');
+      //프로미스가 resolve되면 값을 set
+      setInputValue(e.target.value);
     });
   };
 
